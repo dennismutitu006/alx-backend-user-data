ee4f3369a0b8dbecff74d7c155e1bc7bc85d2974
dennismutitu <dennismutitu06@gmail.com>
2024-07-04 09:13:55 +0000

    check valid password encrypted with bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,11 @@ def hash_password(password: str) -> bytes:
     '''
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''use bcrypt to validate that the provided passwrds macht hashed'''
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False

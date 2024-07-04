5d2f70915a1f6f3ecfbafcd8d0db90ab430dd894
dennismutitu <dennismutitu06@gmail.com>
2024-07-04 07:43:10 +0000

    creating a logger

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -14,11 +14,17 @@ formatter = RedactingFormatter(fields=("email", "ssn", "password"))
 print(formatter.format(log_record))
 
 
-'''filter_datum = __import__('filtered_logger').filter_datum
+filter_datum = __import__('filtered_logger').filter_datum
 
 fields = ["password", "date_of_birth"]
 messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
 
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
-'''
+
+
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))

d82f5121fa2e2cf86109fd8f3551ff4866826def
dennismutitu <dennismutitu06@gmail.com>
2024-07-17 00:01:43 +0300

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -37,3 +38,12 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword args.
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found with the given criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid request with the given ")

cf8b956dff95ead6c1a3ec744a1246dab6affda9
dennismutitu <dennismutitu06@gmail.com>
2024-07-16 22:06:08 +0300

    user class from base class

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''creating an SQLALchemy model named User'''
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    '''the class User for a db table named users
+    the instances will be the rows of the table.
+    '''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        '''to provide a convinent str rep of user objects'''
+        return (f"< User(id={self.id}, email={self.email},"
+                f"hashed_password={self.hashed_password},"
+                f"session_id={self.session_id},"
+                f"reset_token={self.reset_token}) >")

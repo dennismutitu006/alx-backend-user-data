1c8a56d2d311cb59fbe7b3fa404ccc5c5e144747
dennismutitu <dennismutitu06@gmail.com>
2024-07-17 21:48:24 +0300

    with error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,13 @@ def _hash_password(password: str) -> bytes:
     '''
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def valid_login(self, email: str, password: str) -> bool:
+    '''credentials validation'''
+    try:
+        user = self._db.find_user_by(email=email)
+        return bcrypt.checkpw(password.encode('utf-8'),
+                              user.hashed_password('utf-8'))
+    except NoResultFound:
+        return False

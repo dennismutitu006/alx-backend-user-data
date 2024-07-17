91fd3046abb0be1c486a3cceb2014dbd440f428f
dennismutitu <dennismutitu06@gmail.com>
2024-07-17 11:38:12 +0300

    new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,3 +14,6 @@ def welcome():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+@app.route("/users", methods=["POST"])
+def users():

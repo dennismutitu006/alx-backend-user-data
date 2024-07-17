5f761f08cdd81a7d1b4f6703fd2a02056308c188
dennismutitu <dennismutitu06@gmail.com>
2024-07-17 12:06:33 +0300

    attempt

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -31,7 +31,3 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)

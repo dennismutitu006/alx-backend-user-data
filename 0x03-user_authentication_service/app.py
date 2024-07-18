7ee12b4038766af8054c2eef365ed93419593479
dennismutitu <dennismutitu06@gmail.com>
2024-07-18 15:42:23 +0300

    an update for user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,8 +58,6 @@ def profile():
     '''user profile
     '''
     session_id = request.cookies.get("session_id")
-    if session_id is None:
-        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email}), 200

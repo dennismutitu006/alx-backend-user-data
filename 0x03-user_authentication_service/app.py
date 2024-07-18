c8143e4225217be1632a2094a40a8cb8366d624c
dennismutitu <dennismutitu06@gmail.com>
2024-07-18 12:53:34 +0300

    log out and delete session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,5 +40,20 @@ def login():
     return reponse
 
 
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    '''Logout user and destroy session id'''
+    session_id = request.cookies.get("session_id")
+    if session_id is None:
+        return abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        return abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

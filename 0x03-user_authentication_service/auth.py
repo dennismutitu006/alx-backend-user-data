eef895555b061141c5f90dfd7679c8ca7ac3a933
dennismutitu <dennismutitu06@gmail.com>
2024-07-18 11:50:09 +0300

    remove utf-8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,7 +42,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if user:
                 return bcrypt.checkpw(password.encode(),
-                                      user.hashed_password.encode('utf-8'))
+                                      user.hashed_password.encode())
         except NoResultFound:
             return False
 

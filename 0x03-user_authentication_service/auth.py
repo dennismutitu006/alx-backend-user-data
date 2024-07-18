bba73f027af500acfae4e99e713b707575fdabf1
dennismutitu <dennismutitu06@gmail.com>
2024-07-18 10:12:13 +0300

    valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,14 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    '''this method will use gensalt to generated a salted hash of the
+    input password and return the bytes.
+    '''
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -22,20 +30,12 @@ class Auth:
             n_user = self._db.add_user(email, hashed_password.decode('utf-8'))
             return n_user
 
-
-def _hash_password(password: str) -> bytes:
-    '''this method will use gensalt to generated a salted hash of the
-    input password and return the bytes.
-    '''
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
-
-
-def valid_login(self, email: str, password: str) -> bool:
-    '''credentials validation'''
-    try:
-        user = self._db.find_user_by(email=email)
-        return bcrypt.checkpw(password.encode('utf-8'),
-                              user.hashed_password('utf-8'))
-    except NoResultFound:
-        return False
+    def valid_login(self, email: str, password: str) -> bool:
+        '''credentials validation'''
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(password.encode('utf-8'),
+                                      user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
